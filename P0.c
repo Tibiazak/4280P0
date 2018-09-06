d3253c88401566f4b8a00789cc3fdc1e1dea2d53
Joshua Bearden <josh.e.bearden@gmail.com>
2018-09-06 11:55:33 -0500

    added newlines

--- P0.c
+++ P0.c
@@ -8,11 +8,11 @@ int main(int argc, char * argv[])
 {
     if (argc == 1)
     {
-        printf("No file given, read from stdin");
+        printf("No file given, read from stdin.\n");
     }
     else
     {
-        printf("File listed: %s", argv[argc-1]);
+        printf("File listed: %s\n", argv[argc-1]);
     }
     return 0;
 }
