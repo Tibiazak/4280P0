77877dd6c7e61ac69ab84e1eabe7d704f951a4d2
Joshua Bearden <josh.e.bearden@gmail.com>
2018-09-06 11:50:30 -0500

    made initial main structure

---
+++ P0.c
@@ -0,0 +1,16 @@
+//
+// Created by fof_z on 9/6/2018.
+//
+
+int main(int argc, char * argv[])
+{
+    if (argc == 1)
+    {
+        printf("No file given, read from stdin");
+    }
+    else
+    {
+        printf("File listed: %s", argv[argc-1]);
+    }
+    return 0;
+}
